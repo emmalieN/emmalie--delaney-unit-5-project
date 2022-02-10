25d388289213ff6642bbc7be1ed0b5e4fa11c6b7
delaneyolivas <91343478+delaneyolivas@users.noreply.github.com>
2022-02-10 12:28:49 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
-
+let mySprite = sprites.create(img`
+    . . f f f . . . . . . . . . . . 
+    f f f c c . . . . . . . . f f f 
+    f f c c c . c c . . . f c b b c 
+    f f c 3 c c 3 c c f f b b b c . 
+    f f c 3 b c 3 b c f b b c c c . 
+    f c b b b b b b c f b c b c c . 
+    c c 1 b b b 1 b c b b c b b c . 
+    c b b b b b b b b b c c c b c . 
+    c b 1 f f 1 c b b c c c c c . . 
+    c f 1 f f 1 f b b b b f c . . . 
+    f f f f f f f b b b b f c . . . 
+    f f 2 2 2 2 f b b b b f c c . . 
+    . f 2 2 2 2 2 b b b c f . . . . 
+    . . f 2 2 2 b b b c f . . . . . 
+    . . . f f f f f f f . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
